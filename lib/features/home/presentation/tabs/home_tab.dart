669ac924eb9c38fcfeb22f8c78199447af404640
eslam20033 <eeslamahmed2003@GMAIL.COM>
2026-02-22 23:21:36 +0200

    describe changes

--- lib/features/home/presentation/tabs/home_tab.dart
+++ lib/features/home/presentation/tabs/home_tab.dart
@@ -1,4 +1,6 @@
+import 'package:event_hub/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -6,7 +8,42 @@ class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: AppColors.grey,
+      body: Container(
+        padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: AppColors.white,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          'assets/icon/location.svg',
+                          width: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Text('Hello , Eslam Ahmed', style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                CircleAvatar(
+                  backgroundColor: AppColors.white,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset('assets/icon/bell.svg', width: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
